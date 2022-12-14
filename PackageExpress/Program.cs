cf3bb6b9f95f5937ddb221233379d6366b95f225
Nikita Sazonov <sazonovn2018@gmail.com>
2022-12-13 17:50:48 -0700

    new package epress

--- PackageExpress/Program.cs
+++ PackageExpress/Program.cs
@@ -8,24 +8,31 @@ class Program
         int weight = Convert.ToInt32(Console.ReadLine());
         if (weight > 50)
         {
-            throw new ("Package too heavy to be shipped via Package Express. Have a good day");
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day");
             
         }
-        Console.WriteLine("Package Width?");
-        int width = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Package Lenght?");
-        int lenght = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Package Height?");
-        int heigth = Convert.ToInt32(Console.ReadLine());
-
-        int total = (lenght + heigth + width);
-        if (total > 50)
+        else
         {
-            throw new ("Package too big to be shipped via Package Express. Have a good day");
+            Console.WriteLine("Package Width?");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Package Lenght?");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Package Height?");
+            int heigth = Convert.ToInt32(Console.ReadLine());
+
+            int total = (lenght + heigth + width);
+            if (total > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day");
+            }
+            else
+            {
+                decimal money = 100;
+                decimal quote = ((lenght * heigth * width) * weight) / money;
+                Console.WriteLine("Your estimated total for shipping this package is:" + "$" + quote);
+            }
         }
-        decimal money = 100;
-        decimal quote = ((lenght * heigth * width) * weight) / money;
-        Console.WriteLine("Your estimated total for shipping this package is:" + quote);
+        
         Console.ReadLine();
     }
 }
