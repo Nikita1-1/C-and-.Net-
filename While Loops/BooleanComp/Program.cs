350a91186ec11bf7e11e94e8accde3e48a9e85c4
Nikita Sazonov <sazonovn2018@gmail.com>
2022-12-14 08:44:24 -0700

    while loops

---
+++ While Loops/BooleanComp/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace BooleanComp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("This is a do while and while loops");
+
+
+        int num = 5;
+        int count = 0;
+        while (count < num)
+        {
+            Console.WriteLine(count += 1);
+        }
+
+        Console.WriteLine("Guess The Number");
+        int guessed = Convert.ToInt32(Console.ReadLine());
+        bool isGuessed = guessed == 17;
+
+
+        do
+        {
+            switch (guessed)
+            {
+                case 10:
+                    Console.WriteLine("You guessed the number");
+                    Console.WriteLine("Guess The Number");
+                    guessed = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 15:
+                    Console.WriteLine("You guessed the number");
+                    Console.WriteLine("Guess The Number");
+                    guessed = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 17:
+                    Console.WriteLine("You guessed the number");
+                    isGuessed = true;
+                    break;
+            }
+        }
+        while (!isGuessed);
+
+        Console.ReadLine();
+
+
+        
+    }
+}
+
