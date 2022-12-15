4fe00c3e94b0f894ed9fa5308baa7f3440bc0635
Nikita Sazonov <sazonovn2018@gmail.com>
2022-12-15 15:42:06 -0700

    array Assignment

---
+++ array/ArrayOfStrings/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ArrayOfStrings;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] Array = { "hello", "world", "this", "is", "string" };
+        Console.WriteLine("Choose any number between 0-4");
+        int index = Convert.ToInt32(Console.ReadLine());
+        if (index > 4)
+        {
+            Console.WriteLine("There is no such a number");
+        }
+        else
+        {
+            Console.WriteLine(Array[index]);
+        }
+
+        int[] Array2 = { 1,2,3,4,5 };
+        Console.WriteLine("Choose any number between 0-4");
+        int index2 = Convert.ToInt32(Console.ReadLine());
+        if (index2 > 4)
+        {
+            Console.WriteLine("There is no such a number");
+        }
+        else
+        {
+            Console.WriteLine(Array2[index2]);
+        }
+
+
+        string[] cars = { "Mustang", "McLaren", "Buggati", "Pegoe" };
+        List<string> carsRange = new List<string>(cars);
+        Console.WriteLine("Choose any number between 0-4");
+        int index3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(carsRange[index3]);
+
+        Console.ReadLine();
+        }
+}
+
