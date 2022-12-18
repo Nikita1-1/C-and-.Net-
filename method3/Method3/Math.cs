8d4a45905ac884153dabe5e176db6fc6eabee84a
Nikita Sazonov <sazonovn2018@gmail.com>
2022-12-18 16:46:59 -0700

    Commit

---
+++ method3/Method3/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Method3
+{
+	public class Math
+	{
+		public int A { get; set; }
+		public int B { get; set; }
+
+
+
+
+		public int Multiply(int B)
+		{
+            if (B == 0 || B == null)
+            {
+                int C = 2;
+                return A * C;
+            }
+            return A * B;
+		}
+	}
+}
+
---
+++ method3/Method3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Method3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Math operation = new Math();
+
+        Console.WriteLine("please input whole number that is going to multiply by 10");
+        Console.WriteLine("if you insert 0, 10 will be multiply by 10 automaticly");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        operation.A = 10;
+
+        
+        Console.WriteLine(operation.Multiply(B));
+
+        Console.ReadLine();
+    }
+}
+
