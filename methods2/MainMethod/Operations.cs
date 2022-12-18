df01a227441af3f6d979f6fc652d84bfe7154d94
Nikita Sazonov <sazonovn2018@gmail.com>
2022-12-18 16:07:45 -0700

    commit

---
+++ methods2/MainMethod/Operations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace MainMethod
+{
+	public class Operations
+	{
+		public int A { set; get; }
+		public int B { set; get; }
+		public int C { set; get; }
+
+
+		public int doSomething(int B)
+		{
+			return A - B;
+		}
+
+
+		public decimal doSomething(decimal C)
+		{
+			
+			return A / C;
+		}
+
+		public string doSomething(string D)
+		{
+			return A + D;
+		}
+
+    }
+}
+
