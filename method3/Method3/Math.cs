22c5728d63111e885e3ad19455e877e1e262c80c
Nikita Sazonov <sazonovn2018@gmail.com>
2022-12-19 11:58:04 -0700

    commit

--- method3/Method3/Math.cs
+++ method3/Method3/Math.cs
@@ -5,11 +5,13 @@ namespace Method3
 	{
 		public int A { get; set; }
 		public int B { get; set; }
+		
+		public int Num2 { get; set; }
+		public int Num1 { get; set; }
 
 
 
-
-		public int Multiply(int B)
+		public int Multiply(int B)// if B is 0 then default 2 will be applied 
 		{
             if (B == 0 || B == null)
             {
@@ -17,6 +19,15 @@ namespace Method3
                 return A * C;
             }
             return A * B;
+		}
+
+
+		public void twoIntegers(int Num1, int Num2)
+		{
+
+			Console.WriteLine(Num2);
+			Console.WriteLine(Num1 - 1);
+			
 		}
 	}
 }
--- method3/Method3/Program.cs
+++ method3/Method3/Program.cs
@@ -10,9 +10,21 @@ class Program
         int B = Convert.ToInt32(Console.ReadLine());
 
         operation.A = 10;
+        operation.Num1 = 20;
+        int Num2 = 20;
 
+
+
+        Console.WriteLine("please input number 1 and number 2, number 1 will do some math");
         
+
+
         Console.WriteLine(operation.Multiply(B));
+
+        operation.twoIntegers(operation.Num1, Num2);
+
+
+        operation.twoIntegers(Num1: 10, Num2: 10);
 
         Console.ReadLine();
     }
