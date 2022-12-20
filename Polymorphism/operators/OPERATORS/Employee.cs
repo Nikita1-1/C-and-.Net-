1670d891c14a06827c4e51493f362d58a5a31007
Nikita Sazonov <sazonovn2018@gmail.com>
2022-12-20 11:50:11 -0700

    operators

---
+++ Polymorphism/operators/OPERATORS/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace OPERATORS
+{
+	public class Employee
+	{
+		public int ID { get; set; }
+		public string firstName { get; set; }
+		public string lastName { get; set; }
+
+		public static Boolean operator== (Employee employee1, Employee employee2)
+		{
+			if (employee1.ID == employee2.ID)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public static Boolean operator!= (Employee employee1, Employee employee2)
+		{
+            if (employee1.ID != employee2.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+	}
+}
+
---
+++ Polymorphism/operators/OPERATORS/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace OPERATORS;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee employee1 = new Employee();
+        Employee employee2 = new Employee();
+        employee1.ID = 1;
+        employee1.firstName = "Nikita";
+        employee1.lastName = "Sazonov";
+        employee2.ID = 2;
+        employee2.firstName = "Andy";
+        employee2.lastName = "Smith";
+
+        if (employee1 == employee2)
+        {
+            Console.WriteLine("Emplyees are equal");
+        }
+        else
+        {
+            Console.WriteLine("Emplyees are not equal");
+        }
+
+        if (employee1 != employee2)
+        {
+            Console.WriteLine("Emplyees are not equal");
+        }
+        else
+        {
+            Console.WriteLine("Emplyees are equal");
+        }
+
+        Console.ReadLine();
+        
+    }
+}
+
