f5dc8c460560970e07f7db7d8fee182fa2932f1f
Nikita Sazonov <sazonovn2018@gmail.com>
2022-12-19 23:03:15 -0700

    Polym

---
+++ Polymorphism/Abstruction/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Abstruction;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee person = new Employee();
+        person.firstName = "Sample";
+        person.lastName = "Student";
+
+
+        person.sayName();
+
+
+
+
+        IQuittable quit = new Employee();
+
+        quit.Quit();
+
+
+        Console.ReadLine();
+    }
+}
+
