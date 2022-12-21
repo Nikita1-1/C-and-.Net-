475cb8b7691aaf7026ba7147735979267a997d35
Nikita Sazonov <sazonovn2018@gmail.com>
2022-12-21 12:09:07 -0700

    daysOfTheWeek

---
+++ DaysOfTheWeek/DaysOfTheWeek/Days.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DaysOfTheWeek
+{
+	public class Days
+	{
+
+		public allDays daysOfTheWeek { get; set; }
+
+
+        public enum allDays
+		{
+			Monday,
+			Tuesday,
+			Wednesday,
+			Thursday,
+			Friday,
+			Saturday,
+			Sunday
+		}
+		
+	}
+}
+
---
+++ DaysOfTheWeek/DaysOfTheWeek/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+using static DaysOfTheWeek.Days;
+
+namespace DaysOfTheWeek;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // try catch block that is going to catch any errors or wrong input 
+        try
+        {
+
+            // asking user to input day of the week to compare to one in enum
+            Console.WriteLine("please enter current day of the week");
+            string day = Console.ReadLine();
+
+
+
+            // variable with enum type that is parsing enum and takes the value of that if user input is correct and return it
+            allDays dayValue = (allDays)Enum.Parse(typeof(allDays), day);
+
+
+            int d = Convert.ToInt32(day);
+            if (d > 6)
+            {
+                Console.WriteLine("There are only 7 days a week you cannot enter more than number 6");
+                
+            }
+            
+            Console.WriteLine(dayValue);
+
+
+        }
+        catch (FormatException)
+        {
+            // if there was an error(lower case input) then catch block is presenting this message
+            Console.WriteLine("Please Enter actual day");
+           
+        }
+        
+        
+        
+
+        Console.ReadLine();
+    }
+}
+
