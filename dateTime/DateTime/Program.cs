90c0b4593884c9a832392791ed9d0df7142c0903
Nikita Sazonov <sazonovn2018@gmail.com>
2022-12-25 13:16:42 -0700

    commit

---
+++ dateTime/DateTime/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DataTime;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime now = DateTime.Now;
+        now.ToString();
+        Console.WriteLine(DateTime.Now);
+        Console.WriteLine("Enter Number");
+
+        int useriInput = Convert.ToInt32(Console.ReadLine());
+
+        DateTime future = now.AddHours(useriInput);
+        Console.WriteLine(future);
+
+        Console.ReadLine();
+    }
+}
+
