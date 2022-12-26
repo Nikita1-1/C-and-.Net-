422459cfde21c9316e21532aded9847ab0eabecd
Nikita Sazonov <sazonovn2018@gmail.com>
2022-12-26 12:18:52 -0700

    comit

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Constructor;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var newPerson = new InheritConstructor("Nikita");
+        const string person = "Alex";
+        Console.WriteLine(newPerson);
+
+
+        Console.ReadLine();
+
+    }
+}
+
