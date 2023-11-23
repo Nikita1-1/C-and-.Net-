47c5cc90cc57d23319aabd9bc788baea82ec9a58
Nikita <sazonovn2018@gmail.com>
2023-11-22 17:25:23 -0700

    Making Blog MVC Project, will be update soon

---
+++ Blog/Blog.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,79 @@
+﻿using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Rendering;
+namespace Blog.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagRepositories tagRepository;
+        private readonly IBlogPostRepository blogPostRepository;
+
+        public AdminBlogPostsController(ITagRepositories tagRepository, IBlogPostRepository blogPostRepository)
+        {
+            this.tagRepository = tagRepository;
+            this.blogPostRepository = blogPostRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var tags = await tagRepository.GetAllAsync();
+
+            // assining tag to a model.
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(x => new SelectListItem { Text = x.DisplayName, Value = x.Id.ToString() })
+            };
+            //Get tags from repositry
+            return View(model);
+        }
+
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest)
+        {
+
+            // Map view model to domain model
+            var blogPost = new BlogPost
+            {
+                Heading = addBlogPostRequest.Heading,
+                PageTitle = addBlogPostRequest.PageTitle,
+                Content = addBlogPostRequest.Content,
+                ShortDescription = addBlogPostRequest.ShortDescription,
+                FeaturedImageUrl = addBlogPostRequest.FeaturedImageUrl,
+                UrlHandle = addBlogPostRequest.UrlHandle,
+                PublishedDate = addBlogPostRequest.PublishedDate,
+                Author = addBlogPostRequest.Author,
+                Visible = addBlogPostRequest.Visible,
+            };
+            //Map Tags from selected tags
+            var selectedTags = new List<Tag>();
+            foreach(var selectedTagId in addBlogPostRequest.SelectedTags)
+            {
+                var selectedTagIdAsGuid = Guid.Parse(selectedTagId);
+                var existingTag = await tagRepository.GetAsync(selectedTagIdAsGuid);
+
+                if(existingTag != null)
+                {
+                    selectedTags.Add(existingTag);
+                }
+            }
+            blogPost.Tags = selectedTags;
+            await blogPostRepository.AddAsync(blogPost);
+            return RedirectToAction("Add");
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            // call the respository
+
+            var blogPosts = await blogPostRepository.GetAllAsync();
+
+            return View(blogPosts);
+        }
+    }
+}
---
+++ Blog/Blog.Web/Repositories/BlogPostRepository.cs
@@ -0,0 +1,42 @@
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Repositories
+{
+    public class BlogPostRepository : IBlogPostRepository
+    {
+        private readonly BlogDBContext blogDBContext;
+
+        public BlogPostRepository(BlogDBContext blogDBContext)
+        {
+            this.blogDBContext = blogDBContext;
+        }
+        public async Task<BlogPost> AddAsync(BlogPost blogPosts)
+        {
+            await blogDBContext.AddAsync(blogPosts);
+            await blogDBContext.SaveChangesAsync();
+            return blogPosts;
+        }
+
+        public Task<BlogPost?> DeleteAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<BlogPost>> GetAllAsync()
+        {
+            return await blogDBContext.BlogPosts.Include(x => x.Tags).ToListAsync();
+        }
+
+        public Task<BlogPost?> GetAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BlogPost?> UpdateAsync(BlogPost blogPost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
